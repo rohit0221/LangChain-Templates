11f553fd2247012dab9dd3a0ccedd10e2a7441ee
rohit0221 <rohit0221@gmail.com>
2024-04-28 10:17:02 +0530

    Basic Changes to chain.py and server.py

--- rag-conversation/my-app/app/server.py
+++ rag-conversation/my-app/app/server.py
@@ -11,7 +11,8 @@ async def redirect_root_to_docs():
 
 
 # Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+from rag_conversation import chain as rag_conversation_chain
+add_routes(app, rag_conversation_chain, path="/rag-conversation")
 
 if __name__ == "__main__":
     import uvicorn
--- rag-conversation/my-app/packages/rag-conversation/rag_conversation/chain.py
+++ rag-conversation/my-app/packages/rag-conversation/rag_conversation/chain.py
@@ -30,21 +30,21 @@ if os.environ.get("PINECONE_ENVIRONMENT", None) is None:
 PINECONE_INDEX_NAME = os.environ.get("PINECONE_INDEX", "langchain-test")
 
 ### Ingest code - you may need to run this the first time
-# # Load
-# from langchain_community.document_loaders import WebBaseLoader
-# loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
-# data = loader.load()
+# Load
+from langchain_community.document_loaders import WebBaseLoader
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+data = loader.load()
 
 # # Split
-# from langchain_text_splitters import RecursiveCharacterTextSplitter
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-# all_splits = text_splitter.split_documents(data)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
 
 # # Add to vectorDB
-# vectorstore = PineconeVectorStore.from_documents(
-#     documents=all_splits, embedding=OpenAIEmbeddings(), index_name=PINECONE_INDEX_NAME
-# )
-# retriever = vectorstore.as_retriever()
+vectorstore = PineconeVectorStore.from_documents(
+    documents=all_splits, embedding=OpenAIEmbeddings(), index_name=PINECONE_INDEX_NAME
+)
+retriever = vectorstore.as_retriever()
 
 vectorstore = PineconeVectorStore.from_existing_index(
     PINECONE_INDEX_NAME, OpenAIEmbeddings()
